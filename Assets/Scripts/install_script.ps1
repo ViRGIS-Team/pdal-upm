814d68db58ac647e93364ce2fc7bdf2973261a59
Paul <runette@gmail.com>
2020-11-08 14:36:14 +0000

    Update install_script.ps1

--- Assets/Scripts/install_script.ps1
+++ Assets/Scripts/install_script.ps1
@@ -6,17 +6,23 @@ Param(
     ) 
 
 conda create --name upm -y
-conda activate upm
-conda install $install -y --no-deps
+conda install $install --name upm -y --no-deps
 
 
-$env = conda info --json | ConvertFrom-Json 
-$conda_bin = -join($env.active_prefix, "\Library\bin")
+$env = conda info --envs
+$temp = conda info --envs | Select-String  -Pattern 'upm'
+$temp -match "C\:.*"
+$conda_bin = -join($matches[0], "\Library/bin")
+
 Write-Output "Processing *.dll"
 $file = -join($conda_bin, '/*.dll' )
 Write-Output "Copy $file to $destination"
 Copy-Item $file -Destination $destination
 
+Write-Output "Processing $test"
+$file = -join($conda_bin, '/', $test )
+Write-Output "Copy $file to $destination"
+Copy-Item $file -Destination $destination
 
-conda deactivate
+
 conda remove --name upm --all -y
