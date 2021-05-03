f3f0c2ce413cabf7651f6162a2604765d9c43572
Paul <runette@gmail.com>
2021-05-03 17:53:51 +0100

    added mesh to Pointview

--- Runtime/Scripts/PointView.cs
+++ Runtime/Scripts/PointView.cs
@@ -33,6 +33,7 @@ using System.Text;
 using g3;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using System.Linq;
 
 namespace Pdal
  {
@@ -40,48 +41,45 @@ namespace Pdal
 	{
 		private const string PDALC_LIBRARY = "pdalc";
 		private const int BUFFER_SIZE = 1024;
+		private IntPtr mNative = IntPtr.Zero;
 
 		[DllImport(PDALC_LIBRARY, EntryPoint="PDALDisposePointView")]
 		private static extern void dispose(IntPtr view);
 
 		[DllImport(PDALC_LIBRARY, EntryPoint="PDALGetPointViewId")]
-
 		private static extern int id(IntPtr view);
 
 		[DllImport(PDALC_LIBRARY, EntryPoint="PDALGetPointViewSize")]
-
 		private static extern ulong size(IntPtr view);
 
 		[DllImport(PDALC_LIBRARY, EntryPoint="PDALIsPointViewEmpty")]
-
 		private static extern bool empty(IntPtr view);
 
 		[DllImport(PDALC_LIBRARY, EntryPoint="PDALClonePointView")]
-
 		private static extern IntPtr clone(IntPtr view);
 
 		[DllImport(PDALC_LIBRARY, EntryPoint="PDALGetPointViewProj4")]
-
 		private static extern uint getProj4(IntPtr view, StringBuilder buffer, uint size);
 
 		[DllImport(PDALC_LIBRARY, EntryPoint="PDALGetPointViewWkt")]
-
 		private static extern uint getWkt(IntPtr view, StringBuilder buffer, uint size, bool pretty);
 
 		[DllImport(PDALC_LIBRARY, EntryPoint="PDALGetPointViewLayout")]
-
 		private static extern IntPtr getLayout(IntPtr view);
 
 		[DllImport(PDALC_LIBRARY, EntryPoint="PDALGetPackedPoint")]
-
 		private static extern uint getPackedPoint(IntPtr view, IntPtr types, ulong idx, [MarshalAs(UnmanagedType.LPArray)] byte[] buf);
 
 
 		[DllImport(PDALC_LIBRARY, EntryPoint="PDALGetAllPackedPoints")]
-
 		private static extern ulong getAllPackedPoints(IntPtr view, IntPtr types, [MarshalAs(UnmanagedType.LPArray)] byte[] buf);
 
-		private IntPtr mNative = IntPtr.Zero;
+
+		[DllImport(PDALC_LIBRARY, EntryPoint = "PDALGetMeshSize")]
+		private static extern ulong meshSize(IntPtr view);
+
+		[DllImport(PDALC_LIBRARY, EntryPoint = "PDALGetAllTriangles")]
+		private static extern ulong getAllTriangles(IntPtr view, [MarshalAs(UnmanagedType.LPArray)] byte[] buf);
 
 		public PointView(IntPtr nativeView)
 		{
@@ -102,6 +100,11 @@ namespace Pdal
 		public ulong Size
 		{
 			get { return size(mNative); }
+		}
+
+		public ulong MeshSize
+		{
+			get { return meshSize(mNative); }
 		}
 
 		public bool Empty
@@ -154,21 +157,22 @@ namespace Pdal
 			}
 		}
 
-        public byte[] GetAllPackedPoints(DimTypeList dims)
-        {
-            byte[] data = null;
+		public byte[] GetAllPackedPoints(DimTypeList dims, out ulong size)
+		{
+			byte[] data = null;
+			size = 0;
 
 			if (this.Size > 0 && dims != null && dims.Size > 0)
 			{
 				ulong byteCount = this.Size * dims.ByteCount;
 				data = new byte[byteCount];
-				getAllPackedPoints(mNative, dims.Native, data);
+				size = getAllPackedPoints(mNative, dims.Native, data);
 			}
 
 			return data;
-        }
+		}
 
-        public byte[] GetPackedPoint(DimTypeList dims, ulong idx)
+		public byte[] GetPackedPoint(DimTypeList dims, ulong idx)
         {
             byte[] data = null;
 
@@ -180,6 +184,21 @@ namespace Pdal
 
 			return data;
         }
+
+		public byte[] GetPackedMesh(out ulong size)
+		{
+			byte[] data = null;
+			size = 0;
+
+			if (meshSize(mNative) > 0)
+			{
+				ulong byteCount = meshSize(mNative) * 12;
+				data = new byte[byteCount];
+				size = getAllTriangles(mNative, data);
+			}
+
+			return data;
+		}
 
 		public PointView Clone()
 		{
@@ -194,65 +213,99 @@ namespace Pdal
 			return clonedView;
 		}
 
-        public BpcData GetBpcData(long size) {
-            BpcData pc;
-            PointLayout layout = Layout;
-            DimTypeList typelist = layout.Types;
-            byte[] data = GetAllPackedPoints(typelist);
-            List<Vector3d> positions = new List<Vector3d>();
-            List<Vector3f> colors = new List<Vector3f>();
+		public BpcData GetBpcData()
+		{
+			BpcData pc;
+			ulong size;
+			PointLayout layout = Layout;
+			DimTypeList typelist = layout.Types;
+			byte[] data = GetAllPackedPoints(typelist, out size);
+			List<Vector3d> positions = new List<Vector3d>();
+			List<Vector3f> colors = new List<Vector3f>();
 
-            uint pointSize = layout.PointSize;
-            Dictionary<string, int> indexs = new Dictionary<string, int>();
-            Dictionary<string, string> types = new Dictionary<string, string>();
-            int count = 0;
-            bool hasColor = false;
-            for (uint j = 0; j < typelist.Size; j++) {
-                DimType type = typelist.at(j);
-                string interpretationName = type.InterpretationName;
-                int interpretationByteCount = type.InterpretationByteCount;
-                string name = type.IdName;
-				try
-				{
-					if (name == "") name = type.Id.ToString();
-					indexs.Add(name, count);
-                    types.Add(name, interpretationName);
-				}
-				catch { }
+			uint pointSize = layout.PointSize;
+			Dictionary<string, int> indexs = new Dictionary<string, int>();
+			Dictionary<string, string> types = new Dictionary<string, string>();
+			int count = 0;
+			bool hasColor = false;
+			for (uint j = 0; j < typelist.Size; j++)
+			{
+				DimType type = typelist.at(j);
+				string interpretationName = type.InterpretationName;
+				int interpretationByteCount = type.InterpretationByteCount;
+				string name = type.IdName;
+				indexs.Add(name, count);
+				types.Add(name, interpretationName);
 				if (name == "Red")
-                    hasColor = true;
-                count += interpretationByteCount;
-            }
+					hasColor = true;
+				count += interpretationByteCount;
+			}
 
-            for (long i = 0; i < size; i++) {
-                positions.Add( new Vector3d(parseDouble(data, types["X"], (int) (i * pointSize + indexs["X"])), 
-                                            parseDouble(data, types["Y"], (int) (i * pointSize + indexs["Y"])),
-                                            parseDouble(data, types["Z"], (int) (i * pointSize + indexs["Z"]))
-                              ));
-                if (hasColor) 
-                    colors.Add(new Vector3f((float) parseColor(data, types["Red"], (int) (i * pointSize + indexs["Red"])),
-                                            (float) parseColor(data, types["Green"], (int) (i * pointSize + indexs["Green"])),
-                                            (float) parseColor(data, types["Blue"], (int) (i * pointSize + indexs["Blue"]))
-                            ));
-            }
+			for (long i = 0; i < (long)size; i += pointSize)
+			{
+				positions.Add(new Vector3d(parseDouble(data, types["X"], (int)(i + indexs["X"])),
+											parseDouble(data, types["Y"], (int)(i + indexs["Y"])),
+											parseDouble(data, types["Z"], (int)(i + indexs["Z"]))
+							  ));
+				if (hasColor)
+					colors.Add(new Vector3f((float)parseColor(data, types["Red"], (int)(i + indexs["Red"])),
+											(float)parseColor(data, types["Green"], (int)(i + indexs["Green"])),
+											(float)parseColor(data, types["Blue"], (int)(i + indexs["Blue"]))
+							));
+			}
 
 			pc.positions = positions;
 			pc.colors = colors;
 			pc.size = (int)size;
-            return pc;
-        }
+			return pc;
+		}
 
-		public Task<BpcData> GetBpcDataAsync(long size)
+		public Task<BpcData> GetBpcDataAsync()
         {
 			Task<BpcData> t1 = new Task<BpcData>(() =>
 			{
-				return GetBpcData(size);
+				return GetBpcData();
 			});
 			t1.Start();
 			return t1;
         }
 
-        private double parseDouble(byte[] buffer, string interpretationName, int position) {
+		public DMesh3 getMesh()
+		{
+			BpcData pc = GetBpcData();
+
+			ulong size;
+			byte[] data = GetPackedMesh(out size);
+			Console.WriteLine($"Rawmesh size: {size}");
+
+			List<int> tris = new List<int>();
+
+			if (size > 0)
+			{
+				for (int position = 0; position < (int)size; position += 12)
+				{
+					tris.Add((int)BitConverter.ToUInt32(data, position));
+					tris.Add((int)BitConverter.ToUInt32(data, position + 4));
+					tris.Add((int)BitConverter.ToUInt32(data, position + 8));
+				}
+
+			}
+
+			DMesh3 dmesh = DMesh3Builder.Build<Vector3d, int, int>(pc.positions, tris);
+			if (pc.colors.Count() > 0)
+			{
+				dmesh.EnableVertexColors(new Vector3f());
+				foreach (int idx in dmesh.VertexIndices())
+				{
+					dmesh.SetVertexColor(idx, pc.colors.ElementAt(idx));
+				}
+			}
+			return dmesh;
+		}
+
+
+
+		private double parseDouble(byte[] buffer, string interpretationName, int position) {
             double value = 0;
             if (interpretationName == "double") {
                 value = BitConverter.ToDouble(buffer, position);
