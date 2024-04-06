c8ce5a4f2331b01b83085d893f7ed8920dbe32e1
Paul <runette@gmail.com>
2024-04-06 17:39:05 +0100

    Update install_scripts.cs

--- Editor/Scripts/install_scripts.cs
+++ Editor/Scripts/install_scripts.cs
@@ -8,7 +8,7 @@ using Debug = UnityEngine.Debug;
 namespace Pdal {
     public class Install: AssetPostprocessor
     {
-        const string pdalcVersion = "2.2.1";
+        const string pdalcVersion = "2.2.2";
 
         private static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths)
         {
