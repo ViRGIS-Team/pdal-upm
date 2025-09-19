2f2a57a3974f59a398912d89c2ac4cba9126f05c
Paul <runette@gmail.com>
2025-09-19 17:25:50 +0100

    fix gdal metadata

--- Editor/Scripts/install_scripts.cs
+++ Editor/Scripts/install_scripts.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Diagnostics;
 using System;
 using Debug = UnityEngine.Debug;
+using System;
 
 namespace Pdal {
     public class Install: AssetPostprocessor
@@ -48,6 +49,45 @@ namespace Pdal {
             string path = Path.GetDirectoryName(new StackTrace(true).GetFrame(0).GetFileName());
 
             string response = Conda.Conda.Install($"pdal-c={pdalcVersion}");
+
+            string condaLibrary;
+            string condaShared;
+            string condaBin;
+#if UNITY_EDITOR_WIN
+            condaLibrary = Path.Combine(Application.dataPath, "Conda", "Env", "Library");
+            condaShared = Path.Combine(condaLibrary, "share");
+            condaBin = Path.Combine(condaLibrary, "bin");
+#else
+            condaLibrary = Path.Combine(Application.dataPath, "Conda", "Env");
+            condaShared = Path.Combine(condaLibrary, "share");
+            condaBin = Path.Combine(condaLibrary, "lib");
+#endif
+            try
+            {
+                string sharedAssets = Application.streamingAssetsPath;
+                if (Directory.Exists(Path.Combine(condaShared, "gdal")))
+                {
+                    if (!Directory.Exists(sharedAssets)) Directory.CreateDirectory(sharedAssets);
+                    string gdalDir = Path.Combine(sharedAssets, "gdal");
+                    if (!Directory.Exists(gdalDir)) Directory.CreateDirectory(gdalDir);
+                    string projDir = Path.Combine(sharedAssets, "proj");
+                    if (!Directory.Exists(projDir)) Directory.CreateDirectory(projDir);
+
+                    foreach (var file in Directory.GetFiles(Path.Combine(condaShared, "gdal")))
+                    {
+                        File.Copy(file, Path.Combine(gdalDir, Path.GetFileName(file)), true);
+                    }
+
+                    foreach (var file in Directory.GetFiles(Path.Combine(condaShared, "proj")))
+                    {
+                        File.Copy(file, Path.Combine(projDir, Path.GetFileName(file)), true);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                _ = e;
+            }
             Conda.Conda.TreeShake();
         }
     }
